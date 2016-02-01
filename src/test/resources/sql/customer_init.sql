8e651a87fce99cffeb62faee4258f9d9b02eb2f2
zhiyongwu <zhiyongwu@live.cn>
2016-02-01 09:29:38 +0800

    config

--- src/test/resources/sql/customer_init.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO customer(name,contact,telephone,email,remark) VALUES ('customer1','Jack','13512345678','jack@gmail.com',NULL );
-INSERT INTO customer(name,contact,telephone,email,remark) VALUES ('customer2','Rose','13612345678','rose@gmail.com',NULL );
